b229a4609313667e792b8def6ad4cae106d1960c
LolaGM <canal030w@gmail.com>
2023-03-24 11:13:46 +0100

    back Button test OK

--- src/tests/OneCharacter.test.jsx
+++ src/tests/OneCharacter.test.jsx
@@ -1,11 +1,13 @@
 import { render, screen } from '@testing-library/react';
-import OneCharacter from '../components/OneCharacter';
 import { MemoryRouter as Router} from 'react-router-dom';
+import OneCharacter from '../components/OneCharacter'
 
-/*Aquí interfieren rutas en este botón que testeamos, por eso vamos a testearlo así que debemos importar también el ROUTER DOM*/
 test('render backButton', ()=>{
-    render(<Router><OneCharacter/><Router/>);
+    render(<Router><OneCharacter/></Router>)
     const backButton = screen.getByText(/Back/i);
     expect(backButton).toBeInTheDocument();
 });
+
+/*Aquí interfieren rutas en este botón que testeamos, por eso vamos a testearlo así que debemos importar también el ROUTER DOM*/
+
 
